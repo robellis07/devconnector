25a85f37e6dad6ac265efd1242faa29913b8f031
robellis07 <elllrob@hotmail.com>
2019-12-17 22:33:27 -0500

    updates

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,23 +6,22 @@ const User = require('../../models/User');
 // @route   GET api/auth
 // @desc    test route
 // @access  public
-router.get('/', auth, (req, res) => {
+router.get('/', auth, async (req, res) => {
 
   console.log(`Attempting to find ${ req.user.id }`);
   try {
-    User.findById(req.user.id)
-    .then(theUser => {
-      if (!theUser) {
-        return res
-          .status(401)
-          .send({msg : 'Unable to find user'});
-      }
-
+    const theUser = await User.findById(req.user.id);
+    if (!theUser) {
       return res
-        .status(200)
-        .send(`Welcome ${ theUser.name }.  Your email address is ${ theUser.email }.`);
-    });
-  } catch(err) {
+        .status(401)
+        .send({msg : 'Unable to find user'});
+    }
+
+    return res
+      .status(200)
+      .send(`Welcome ${ theUser.name }. Your email address is ${ theUser.email }`);
+
+    } catch(err) {
     throw err;
   }
 });
